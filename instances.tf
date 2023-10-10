ae0b521098652cfebdbeb95cd1db11e20f84330a
roni1990 <115533867+roni1990@users.noreply.github.com>
2023-10-10 23:21:40 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -4,13 +4,13 @@ resource "aws_instance" "vm" {
   count = 3
   user_data = <<-EOF
               #!/bin/bash
-              adduser mastora
-              mkdir -p /home/mastora/.ssh
-              touch /home/mastora/.ssh/authorized_keys
-              echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbXvaCsH2I5o5qAn2TB/rhnEX68oLD3qegA/UNqipAfH8wslIuUFk561BDSRKrrIYCZjJvyafR56TrTc6YBDNy8B2fv5Z0VvwdDDVamC032/Wga4tL4+IN56lOVui4a8AB1jIjGIhxiOYpVQmqjmaCEXXN58yRHOBrxaSJLGGVP2itf9lpyRH85aOo+MuDQGjqOpXPTKnQoAjwOZFXW+aTnpnVDZdQkWwEJ7jLiw8+JvdX76uZxQxoksyFnp3LJL6pupI+vmvYvT67BpIwH+EMY2d0nrtIgvWPayj2IlTgzmFN9sB96A10/cV7D4CnHcTce7/tg8KMJ/jLB6H6Hr0/ Eljano@SKANDERBEG" > /home/mastora/.ssh/authorized_keys
-              chmod 700 /home/mastora/.ssh
-              chmod 600 /home/mastora/.ssh/authorized_keys
-              chown -R mastora:mastora /home/mastora/.ssh
+              adduser erjon
+              mkdir -p /home/erjon/.ssh
+              touch /home/erjon/.ssh/authorized_keys
+              echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbXvaCsH2I5o5qAn2TB/rhnEX68oLD3qegA/UNqipAfH8wslIuUFk561BDSRKrrIYCZjJvyafR56TrTc6YBDNy8B2fv5Z0VvwdDDVamC032/Wga4tL4+IN56lOVui4a8AB1jIjGIhxiOYpVQmqjmaCEXXN58yRHOBrxaSJLGGVP2itf9lpyRH85aOo+MuDQGjqOpXPTKnQoAjwOZFXW+aTnpnVDZdQkWwEJ7jLiw8+JvdX76uZxQxoksyFnp3LJL6pupI+vmvYvT67BpIwH+EMY2d0nrtIgvWPayj2IlTgzmFN9sB96A10/cV7D4CnHcTce7/tg8KMJ/jLB6H6Hr0/ Eljano@SKANDERBEG" > /home/erjon/.ssh/authorized_keys
+              chmod 700 /home/erjon/.ssh
+              chmod 600 /home/erjon/.ssh/authorized_keys
+              chown -R erjon:erjon /home/erjon/.ssh
               EOF
 }
 
